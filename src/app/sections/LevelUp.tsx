c630a9d75680765ef049891afb3902d874c71c8a
Muhammad A <muhammada7268@gmail.com>
2024-12-06 15:16:54 +0500

    Complete Levelup Section

--- src/app/sections/LevelUp.tsx
+++ src/app/sections/LevelUp.tsx
@@ -10,14 +10,20 @@ const LevelUp = () => {
 	return (
 		<div className="py-[30px] mt-[70px]">
 			<div className="flex flex-col">
-				<h1 className="text-center text-[48px] leading-[57.6px] font-extrabold from-[#555555] to-black bg-gradient-to-b bg-clip-text text-transparent">Level up your quality of work</h1>
+				<h1 className="text-center text-[40px] md:text-[42px] lg:text-[48px] leading-[104%] font-extrabold from-[#555555] to-black bg-gradient-to-b bg-clip-text text-transparent">
+					Level up your&nbsp;
+					<span className="from-[#555555] to-black bg-gradient-to-b bg-clip-text font-extrabold relative after:absolute after:bottom-0 after:left-0 after:w-[100%] after:translate-y-[40%] after:h-[30%] after:bg-contain after:bg-no-repeat after:bg-[url('/font_underline.png')] after:-z-20">
+						quality&nbsp;
+					</span>
+					of work
+				</h1>
 				<h2 className="px-[50px] text-center mt-4 text-[20px] leading-[27px] text-[#666666] max-w-[1070px] mx-auto">
 					Lorem ipsum dolor sit amet consectetur. Rhoncus porttitor velit bibendum rutrum pharetra semper. Cursus nunc ultrices nulla dapibus purus semper penatibus sit quis. Morbi amet
 					gravida scelerisque proin amet morbi.
 				</h2>
 			</div>
 			<div className="relative">
-				<div className="mt-[50px] flex justify-center w-[95%] lg:w-[970px] h-[560px] mx-auto rounded-[20px] bg-white shadow-[0px_10px_25px_-3px_#00000026] overflow-hidden">
+				<div className="mt-[50px] flex justify-center w-[95%] lg:w-[970px] h-[400px] lg:h-[560px] mx-auto rounded-[20px] bg-white shadow-[0px_10px_25px_-3px_#00000026] overflow-hidden">
 					<LogoBackImg className="absolute -translate-x-[50%] -translate-y-[50%] left-[50%] top-[50%] hidden lg:block" />
 					<LogoMainImg className="absolute -translate-x-[50%] -translate-y-[50%] left-[50%] top-[50%]" />
 					<button className="absolute left-[50%] -translate-x-[50%] bottom-[40px] hover:-translate-y-1 duration-200 ease-in-out">
